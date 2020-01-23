f2ebf7c93d4e8842996f33b15c34aeea7ca9576b
thomas-crane <thomasgcrane@gmail.com>
2020-01-23 21:23:54 +1100

    feat(types): add diagnostic types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,76 @@
 /**
+ * Types of diagnostics which can be generated.
+ */
+export enum DiagnosticKind {
+  Message,
+  Warning,
+  Error,
+  Hint,
+}
+
+/**
+ * Sources of diagnostic messages.
+ */
+export const enum DiagnosticSource {
+  Lexer = 0,
+  Parser = 1 << 1,
+  Checker = 1 << 2,
+}
+
+/**
+ * The base type of all types which represent some kind of diagnostic.
+ */
+interface Diagnostic {
+  kind: DiagnosticKind;
+  source: DiagnosticSource;
+}
+
+/**
+ * A diagnostic message, typically just useful information
+ * that is not an error or a hint.
+ */
+export interface DiagnosticMessage extends Diagnostic, TextRange {
+  kind: DiagnosticKind.Message;
+  message: string;
+}
+
+/**
+ * A diagnostic message about something that is not a problem
+ * now but could become one in the future.
+ */
+export interface DiagnosticWarning extends Diagnostic, TextRange {
+  kind: DiagnosticKind.Warning;
+  warning: string;
+}
+
+/**
+ * A diagnostic message about something which has gone wrong
+ * and from which the system cannot continue.
+ */
+export interface DiagnosticError extends Diagnostic, TextRange {
+  kind: DiagnosticKind.Warning;
+  error: string;
+}
+
+/**
+ * A diagnostic message suggesting a change or action to take.
+ */
+export interface DiagnosticHint extends Diagnostic, TextRange {
+  kind: DiagnosticKind.Hint;
+  hint: string;
+}
+
+/**
+ * The set of all diagnostic types.
+ */
+export type DiagnosticType
+  = DiagnosticMessage
+  | DiagnosticWarning
+  | DiagnosticError
+  | DiagnosticHint
+  ;
+
+/**
  * A slice of text.
  */
 export interface TextRange {
